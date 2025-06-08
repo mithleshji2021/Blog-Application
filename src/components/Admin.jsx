13120026bb5d532053a8160bf323761b36965022
Mithlesh Kumar <mithleshji2021@gmail.com>
2025-06-09 00:21:06 +0530

    admin text color change

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Admin() {
   return (
-    <div className='text-red-600'>Only admin can do : create post, update post & delete post</div>
+    <div className='text-blue-600'>Only admin can do : create post, update post & delete post</div>
   )
 }
 
