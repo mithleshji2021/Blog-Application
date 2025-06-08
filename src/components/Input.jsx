88b4a3cf4648f97b91b3886438d8d2e21ade9d29
Mithlesh Kumar <mithleshji2021@gmail.com>
2025-06-08 23:04:00 +0530

    small changes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -17,7 +17,7 @@ const Input = forwardRef(function Input({
 
             <input 
             type={type}
-            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}  `}
+            className={`px-3 py-2 rounded-lg  outline-none  duration-200 border border-gray-200 w-full ${className}  `}
             id={id}
             ref={ref}
             {...props}
