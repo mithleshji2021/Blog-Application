1c8ac73cb58c63c7ab1985c108d1001a74de9aad
Mithlesh Kumar <mithleshji2021@gmail.com>
2025-06-10 13:21:35 +0530

    update in admin comp

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function Admin() {
-  return (
-    <div className='text-blue-600'>Only admin can do : create post, update post & delete post</div>
-  )
+
+  const userStatus = useSelector((state) => state.auth.status)
+  if (userStatus) {
+    return (
+      <div className='text-blue-600'>Only admin can do : create post, update post & delete post</div>
+    )
+  }
 }
 
 export default Admin
